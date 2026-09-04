7ef036b0fa5af02ed4dd69d181b3f75905847a9c
ianmadariaga07 <iantranquis11@gmail.com>
2026-09-03 19:27:43 -0600

    feat(pizzaservice): uso de placeholder, parametros en una url y creamos la funcion obtener pizza

--- TEP/PizzasService/index.js
+++ TEP/PizzasService/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000; // Puerto en el que escuchará el servidor
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.get("/", (req, res)=>{
     const respuesta = { mensaje: "BIENVENIDO A PIZZA PLANETA"}
     return res.json(respuesta)
@@ -37,6 +40,42 @@ app.get("/api/v1/pizza/bebidas", (req, res)=>{
         respuesta: "La eleccion fue exitosa, pedido completado. Continue con el metodo de pago"
     }
     return res.status(200).json(respuesta)
+})
+
+app.get("/api/v1/pizza/:id", (req, res)=>{
+    console.log(req.query)
+    console.log(req.path)
+    console.log(req.params)
+    const id = req.params.id
+    console.log("id: ", id)
+    const pizzas = [{ nombre: "Hawaiana", descripcion: "Jamon y Piña"}]
+    return res.status(200).json(pizzas)
+})
+
+
+/*
+regresa una pizza por el id
+esta es una simulacion para la conexion de la db
+@param {*} id
+*/
+
+function obtenerPizzaID(id){
+    return { nombre: "Hawaiana", descripcion: "Jamon y Piña" }
+}
+
+app.get("/api/v1/pizza/function/:id", (req, res)=>{
+    const id = req.params.id
+    console.log("id: ", id)
+    const pizzas = obtenerPizzaID(id)
+    return res.status(200).json(pizzas)
+})
+
+//consumo de otro servicio, fetch
+app.get("/api/v1/placeholder", async (req, res)=>{
+    const response = await fetch("https://jsonplaceholder.typicode.com/post/1")
+    const json = await response.json()
+
+    return res.status(200).json(json)
 })
 
 // Iniciar el servidor
