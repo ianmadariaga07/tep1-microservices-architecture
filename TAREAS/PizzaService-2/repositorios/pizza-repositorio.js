2ac4370a66b3aed91e4cf8b784da36eccf25680d
ianmadariaga07 <iantranquis11@gmail.com>
2026-09-07 23:47:58 -0600

    feat(PizzaService-2): metodos actualizar y borrar pizza dentro del repositorio de pizzas

--- TAREAS/PizzaService-2/repositorios/pizza-repositorio.js
+++ TAREAS/PizzaService-2/repositorios/pizza-repositorio.js
@@ -13,3 +13,25 @@ export async function obtenerPizzaIdAsync(id){
     const pizza = pizzas.find(x => x.id == id)
     return pizza
 }
+
+export async function agregarPizzaAsync(pizza){
+    await sleep(1000)
+    pizzas.push(pizza)
+}
+
+//ACTUALIZAR
+export async function actualizarPizzaAsync(id, pizzaActualizada){
+    await sleep(1000)
+    let pizza = pizzas.find(x => x.id == id)
+    pizza.nombre = pizzaActualizada.nombre
+    pizza.descripcion = pizzaActualizada.descripcion
+    return "pizza actualizada"
+}
+
+//BORRAR
+export async function borrarPizzaAsync(id){
+    await sleep(1000)
+    const pizza = pizzas.findIndex(x => x.id == id)
+    pizzas.splice(pizza,1)
+    return "pizza borrada"
+}
